bcf298e4f2aabcacd55f8936fe78aba9577ed436
get-itips <andresg@capazero.net>
2018-05-09 17:43:29 -0300

    minor changes

--- VSTSPS/Public/Get-VSTSProjectProperties.ps1
+++ VSTSPS/Public/Get-VSTSProjectProperties.ps1
@@ -3,7 +3,7 @@ Function Get-VSTSProjectProperties {
 .SYNOPSIS
     Retrieve the list of VSTS/TFS Project Properties based on Project Id
 .NOTES
-    N/A
+    Strange enough, using Invoke-WebRequest, retrieves more properties than using Invoke-RestMethod
 #>
 
     [cmdletBinding()]
@@ -11,21 +11,17 @@ Function Get-VSTSProjectProperties {
         [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
         [pscustomobject]$VSTSSession,
 
-        [switch]$Raw = $false,
-
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
         [string]$Id=''
     )
     begin {
         $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $VSTSSession.User,$VSTSSession.Token)))
-        $headers = @{}
-        $headers.Add("Authorization","Basic " + $EncodedText)  
         $uri = "https://" + "$($VSTSSession.VSTSAccount).VisualStudio.com/_apis/projects/"+"$($Id)/properties?api-version=5.0-preview.1"  
     }
     process {
 
         $wr= Invoke-RestMethod -Method GET -Uri $uri -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -ContentType "application/json"
 
-        $wr  | select -expandproperty value
+        $wr  | Select-Object -expandproperty value
     }
 }
